9b0e0c7095151e58a23641de5791c2e2da0e09eb
Rizki <104330887+MRizki28@users.noreply.github.com>
2023-03-10 14:11:26 +0700

    create url

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/pengeluaran', [BazarController::class, 'index']);
+Route::get('/bazar', [BazarController::class, 'index']);
+Route::post('/bazar/create', [BazarController::class, 'store']);
