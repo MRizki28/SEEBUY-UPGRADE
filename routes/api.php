2dfa594d92ca0fb2c6358ded0c294083ac666e7d
Rizki <muhammadrizkitkj123@gmail.com>
2023-03-27 12:28:25 +0700

    create url api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::get('cms/bazar', [BazarController::class, 'index'])->name('getData.bazar');
+Route::post('cms/bazar/create', [BazarController::class, 'store'])->name('tambahData.bazar');
+Route::get('cms/bazar/{id}', [BazarController::class, 'showById'])->name('editData.bazar');
+Route::put('cms/bazar/update/{id}', [BazarController::class, 'update'])->name('updateData.bazar');
+Route::delete('cms/bazar/delete/{id}', [BazarController::class, 'delete'])->name('deleteData.bazar');
