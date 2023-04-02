4733349428319da6c0db528611f5432ce357411d
Rizki <muhammadrizkitkj123@gmail.com>
2023-04-02 09:25:30 +0700

    create template view for verification email

---
+++ resources/views/emails/verification.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email Verification</title>
+</head>
+<body>
+    <p>Silahkan klik link verifikasi di bawah:</p>
+    <a href="{{ $verificationUrl }}">Verify Email Address</a>
+    <br>
+    <p>JIka anda tidak melakukan verifikasi anda tidak bisa login</p>
+    <br>
+    <p>Terimakasih </p>
+</body>
+</html>
