f86af1993a0f8f5886a591bdb17394dcef3eaefc
Rizki <muhammadrizkitkj123@gmail.com>
2023-04-02 09:23:07 +0700

    create email verification

--- app/Http/Controllers/AUTH/AuthController.php
+++ app/Http/Controllers/AUTH/AuthController.php
@@ -3,49 +3,112 @@
 namespace App\Http\Controllers\AUTH;
 
 use App\Http\Controllers\Controller;
+use App\Mail\VerificationMail;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
-use Auth;
 
 class AuthController extends Controller
 {
-   public function register(Request $request)
-   {
-    $validator = Validator::make($request->all(),[
-        'name' => 'required',
-        'email' => 'required',
-        'password' => 'required',
-        'konfirmasi_password' => 'required|same:password',
-    ]);
-
-    if ($validator->fails()) {
-        return response()->json([
-            'message' => 'check your validation',
-            'errors' => $validator->errors()
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'password' => 'required',
         ]);
-    }
 
-    try {
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'check your validation',
+                'errors' => $validator->errors()
+            ]);
+        }
+
         $data = new User();
         $data->name = $request->name;
         $data->email = $request->email;
         $data->password = Hash::make($request->password);
         $data->save();
-    } catch (\Throwable $th) {
+
+        // send verification email
+        $this->sendVerificationEmail($data);
+
+        $token = $data->createToken('auth_token')->plainTextToken;
         return response()->json([
-            'message' => 'failed create user',
-            'message' => $th->getMessage()
+            'message' => 'success create user',
+            'data' => $data,
+            'access_token' => $token,
+            'type_token' => 'Bearer'
+        ]);
+    }
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Unauthorized',
+                'code' => 401
+            ]);
+        }
+
+        $user = User::where('email', $request['email'])->first();
+        if (!$user || !$user->email_verified_at) {
+            Auth::logout();
+            return response()->json([
+                'message' => 'Email not verified',
+                'code' => 401
+            ]);
+        }
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'success create user',
+            'data' => $user,
+            'access_token' => $token,
+            'type_token' => 'Bearer'
         ]);
     }
 
-    $token = $data->createToken('auth_token')->plainTextToken;
-    return response()->json([
-        'message' => 'success create user',
-        'data' => $data,
-        'access_token' => $token,
-        'type_token' => 'Bearer'
-    ]);
-   }
+
+
+    public function verifyEmail(Request $request)
+    {
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        if ($user->email_verified_at) {
+            return response()->json([
+                'message' => 'Email already verified',
+                'code' => 400
+            ]);
+        }
+
+        $user->email_verified_at = now();
+        $user->save();
+
+        return response()->json([
+            'message' => 'Email verified successfully',
+            'code' => 200
+        ]);
+    }
+
+    private function sendVerificationEmail(User $user)
+    {
+        $verificationUrl = url('/verify-email/' . $user->email);
+
+        Mail::to($user->email)->send(new VerificationMail($verificationUrl));
+
+        return response()->json([
+            'message' => 'Success sending verification email',
+            'code' => 200
+        ]);
+    }
+
+
 }
