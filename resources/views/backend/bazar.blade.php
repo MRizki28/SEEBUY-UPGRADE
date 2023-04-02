6c0788d82d4227ab77aee4c2bf15c13d68b296a9
Rizki <muhammadrizkitkj123@gmail.com>
2023-04-02 09:24:40 +0700

    change cdn jquery

--- resources/views/backend/bazar.blade.php
+++ resources/views/backend/bazar.blade.php
@@ -126,10 +126,7 @@
         </div>
     </div>
 
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"
-        integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
